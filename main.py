2244b97d430eead28ad5ae9c303361a1b2e20cd0
lopsig <jonathan.ls.trabajo@gmail.com>
2025-09-22 23:04:32 -0500

    cors

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://brewsexpress.netlify.app",
-    "https://brews-express-backend.onrender.com"
+    "https://brews-express-backend.onrender.com",
+    "https://brews-express-backend.onrender.com/brews_express/all_brews"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=origins,
-    allow_origins=["*"],
+    allow_origins=origins,
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
