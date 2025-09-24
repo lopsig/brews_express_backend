26b3fa0290ea69ee070609e420d918a4f45264be
lopsig <jonathan.ls.trabajo@gmail.com>
2025-09-23 23:44:28 -0500

    origins para azure

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://brewsexpress.netlify.app",
-    "https://brews-express-backend.onrender.com"
+    "https://brews-express-backend.onrender.com",
+    "https://beersstoragepython.blob.core.windows.net"
 ]
 
 app.add_middleware(
