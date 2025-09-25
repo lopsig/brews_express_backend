740ee126bfa42adeec9989a54df632f9b0920f5b
lopsig <jonathan.ls.trabajo@gmail.com>
2025-09-24 19:50:51 -0500

    Cors netlify

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://brewsexpress.netlify.app",
-    "https://brews-express-backend.onrender.com"
+    "https://brews-express-backend.onrender.com",
+    "https://brewsexpress.netlify.app"
 ]
 
 app.add_middleware(
